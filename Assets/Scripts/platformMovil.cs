fd36a4c1b20c496bf9c492f5e256d04754d2b5c7
Cristian Arias <zanardy20@hotmail.com>
2020-08-31 06:39:50 -0500

    plataforma movil

---
+++ Assets/Scripts/platformMovil.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformMovil : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag =="Player" && other is BoxCollider2D){
+            GameObject.Find("Player").GetComponent<Transform>().SetParent(this.transform);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.tag =="Player" && other is BoxCollider2D){
+            GameObject.Find("Player").GetComponent<Transform>().parent =null;
+        }
+    }
+}
